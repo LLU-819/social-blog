3c0990a6d7e9e59553cbc30f1347789529ff8bf4
LLU <luz671948@gmail.com>
2024-01-23 23:35:16 +0800

    feature: routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import LeftBar from "./pages/components/leftBar/LeftBar.jsx";
+import RightBar from "./pages/components/rightBar/RightBar.jsx";
+import NavBar from "./pages/components/navBar/NavBar.jsx";
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
+import Home from "./pages/home/Home.jsx";
+import Profile from "./pages/profile/Profile.jsx";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+
+
 function App() {
-  return <div>Hello World!</div>;
+  const Layout = ()=>{
+    return (
+      <div>
+        <NavBar />
+        <div style={{display: 'flex'}}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="profile/:id" element={<Profile />} />
+      </Route>
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+        {/* <Route path=":id" element={<UserProfile />} /> */}
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
---
+++ src/pages/components/navBar/NavBar.jsx
@@ -0,0 +1,7 @@
+import './navBar.scss'
+
+const NavBar =()=>{
+    return <div className='navBar'>navBar</div>
+}
+
+export default NavBar
